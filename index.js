40b9fc19eab86d2618a77758c7bd85f9c25bf105
Mohammad Mehdi <84872648+Mohammad-mahdi2040@users.noreply.github.com>
2021-07-25 14:41:44 +0430

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const {
     video_urls,
     owner,
     prefix,
-    status
+    status,
+    membervc,
+    countmember
 } = require("./config.json")
 const client = new Discord.Client()
 
@@ -16,8 +18,8 @@ client.on("ready", () => {
     client.user.setPresence({
     status: (status),
     activity: {
-        name: `FA_NETWORK STUDIO`,
-        type: 'PLAYING',// COMPETING STREAMING LISTENING WATCHING PLAYING <CUSTOM_STATUS>
+        name: ``,
+        type: 'CUSTOM_STATUS',// COMPETING STREAMING LISTENING WATCHING PLAYING <CUSTOM_STATUS>
     }
   })
     const voiceChannel = client.channels.cache.get(channel_id)
@@ -34,6 +36,21 @@ client.on("ready", () => {
         play(connection)
     })
 })
+
+client.on("ready", () => {
+    function status() {
+        const micneshin = client.guilds.cache.get(membervc)
+        const membercount = client.guilds.cache.get(countmember)
+        const voiceChannels = micneshin.channels.cache.filter(c => c.type === 'voice');
+        let count = 0;
+
+        for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
+        let go = ["Text 1" , "Text 2",`${count}`,`${membercount.memberCount}`]
+        let plsc = ["Format 1","Format 2"]
+        let Power = Math.floor(Math.random() * go.length);
+        client.user.setActivity(go[Power], {type: plsc[Power]});
+      }; setInterval(status, 2900)
+    });
 
 client.on('message', message => {
       if (message.content === `${prefix}reset`) {
